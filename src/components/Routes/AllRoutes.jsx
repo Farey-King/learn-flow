075217bbeb0c4e3f066ae2eb13403ea2b23fe422
maryamsendoo <marymsen05@gmail.com>
2025-07-19 14:24:33 +0100

    first commit

---
+++ src/components/Routes/AllRoutes.jsx
@@ -0,0 +1,14 @@
+// src/components/Routes/AllRoutes.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import SignUp from '../auth/SignUp';
+
+export default function AllRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+}
